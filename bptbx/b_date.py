f3c35f5247cc1583d2f27119bc5e791f5846ae07
Basti Tee <basti.tee@gmx.de>
2017-08-04 09:56:37 +0200

    print-intervals-method

--- bptbx/b_date.py
+++ bptbx/b_date.py
@@ -4,6 +4,7 @@ References for format strings:
 """
 
 from datetime import datetime
+from dateutil.relativedelta import *
 import time
 
 DEFAULT_CONVERT_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
@@ -68,3 +69,21 @@ def get_current_date_for_filename():
     """Returns a date timestamp that can be used for filenames"""
 
     return datetime.fromtimestamp(time.time()).strftime('%Y%m%d')
+
+
+def print_intervals(start_timestamp, end_timestamp,
+                    interval_sec=relativedelta(months=+1), input_format=DEFAULT_CONVERT_FORMAT,
+                    output_format=DEFAULT_CONVERT_FORMAT,
+                    list_format='{} {}'):
+    """Print one interval per line for the given time span."""
+    start = timestamp_to_dto(start_timestamp, input_format)
+    end = timestamp_to_dto(end_timestamp, input_format)
+    start_iv = start
+    while True:
+        end_iv = start_iv + interval_sec
+        print(list_format.format(
+            dto_to_timestamp(start_iv, output_format),
+            dto_to_timestamp(end_iv, output_format)))
+        if end_iv >= end:
+            break
+        start_iv = end_iv
